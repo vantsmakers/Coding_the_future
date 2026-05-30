53b1669d7f4e2029836c7a34a9dfe18743648a09
vantsmakers <vants.makers.club@gmail.com>
2026-05-30 10:21:17 -0600

    Add basic LCD display setup with I2C
    Initialize I2C LCD and display messages.

---
+++ Pantalla.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+
+#include <LiquidCrystal_I2C.h>
+
+
+// I2C address: 0x27 or 0x3F
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void setup() {
+
+    lcd.init();
+
+    lcd.backlight();
+
+
+    lcd.setCursor(0, 0);
+
+    lcd.print("Hello, World!");
+
+
+    lcd.setCursor(0, 1);
+
+    lcd.print("LCD with I2C");
+
+}
+
+
+void loop() {
+
+}
