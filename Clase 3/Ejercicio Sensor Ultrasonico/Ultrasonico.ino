cb0ac89dcb9a424c5d342875a36b48b0793b1372
vantsmakers <vants.makers.club@gmail.com>
2026-09-18 23:38:30 -0600

    Create Ultrasonico.ino

---
+++ Clase 3/Ejercicio Sensor Ultrasonico/Ultrasonico.ino
@@ -0,0 +1,29 @@
+const int trigPin = 9; 
+const int echoPin = 10; 
+
+void setup() {
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  long duration = pulseIn(echoPin, HIGH);// Variable para almacenar el tiempo de ida y vuelta del pulso
+
+  int distancia = duracion / 58.2;; // Variable para almacenar la distancia calculada
+  Serial.print("Distancia: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);
+}
